00a6526c48bc71b55195dd23fb3dc2eb2c259ed6
metoto <hj9706@163.com>
2019-03-11 22:29:31 +0800

    add comments

--- Script/test.py
+++ Script/test.py
@@ -13,6 +13,18 @@ class test:
         pass
 
 if __name__ == "__main__":
+
+
+
+    # 当前的思路如下
+    # 1. 在网上找到当天发布的一些内容（母语内容，如英文或者中文）
+    # 2. 将母语内容填充在excel表里做为原文，然后通过调用机翻，将原文翻译为各语种的译文，并保存在excel表里
+    # 3. 将各语种的译文作为模板的内容部分，“填充”到word模板里，并保存为id+langId的名称格式
+
+
+    # 1. path是excel表的路径，sheetName是数据存放的tab名
+    # 2. tempPath是word模板的路径，destFolder_path是生成的新的word文件保存的路径
+
     path=u"E:\\_测试组文件\\测试资源\\机翻测试.xlsx"
     sheetName = u'word文档翻译原文生成'
     tempPath = u"E:\\_00000\\TemplateWord.docx"
@@ -21,7 +33,7 @@ if __name__ == "__main__":
 
 
 
-
+    #读取excel表，并获取原文，text1到text4（可自行扩展）
     workbook = ExcelUtils.ExcelDriver(path)
     worksheet = workbook.getWorksheet(sheetName)
 
@@ -31,6 +43,8 @@ if __name__ == "__main__":
     text4 = ExcelUtils.ExcelDriver.getCellData(2, 7, worksheet)
     print text1
 
+
+    #使用外部谷歌机翻进行翻译，并将译文填写到excel里，填写完成后保存Excel
     js=GoogleTranslateUtils.Py4Js()
     tk1 = js.getTk(text1)
     tk2 = js.getTk(text2)
@@ -59,6 +73,8 @@ if __name__ == "__main__":
     workbook.saveExcelFile(path)
     #print TempCell
 
+
+    #再通过遍历Excel，将刚刚保存的各语种的译文通过word模板，以context为规则填充到模板里，并保存到新的文件路径
     for index in range(num-1):
         langId = workbook.getCellData(index + 3, 3, worksheet)
         caseId = workbook.getCellData(index + 3, 1, worksheet)
